0f1d7097598b3724334d6304d30ac28f80943841
killeen31 <j31killeen@gmail.com>
2023-02-08 15:34:46 -0500

    more changes to js

--- script.js
+++ script.js
@@ -3,22 +3,28 @@ let lower = "abcdefghijklmnopqrstuvwxyz";
 let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let number = "1234567890";
 let special = "!@#$%^*(){}[]:;'";
-let includeLower = false; 
+let includeLower = false;
 let includeUpper = false;
 let includeNumber = false;
 let includeSpecial = false;
 let passwordLength = 0;
-let validCharacters = "";
+
 
 
 function generatePassword() {
+  let validCharacters = "";
   console.log("I am here");
-passwordLength = prompt("enter password length between 8 and 128");
-passwordLength = parseInt(passwordLength);
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("password length must be between 8 and 128 please re-try");
-  generatePassword();
-}
+  passwordLength = prompt("enter password length between 8 and 128");
+  passwordLength = parseInt(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("password length must be between 8 and 128 please re-try");
+    generatePassword();
+  }
+  includeLower = confirm("okay to include lower case characters in password");
+  if (includeLower) {
+    validCharacters += lower;
+  }
+  console.log("valid characters = ", validCharacters);
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -39,8 +45,8 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // Generator function 
-function getRandomlower () {
-  return String. fromCharCode();
+function getRandomlower() {
+  return String.fromCharCode();
 }
 
 function getRandomUpper() {
